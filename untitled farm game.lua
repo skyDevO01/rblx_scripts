4f32e5293fed0b74e116e13fa19d8cc46aedc96d
ngb <songhyedork@gmail.com>
2025-07-18 19:31:45 +0700

    Create untitled farm game.lua

---
+++ untitled farm game.lua
@@ -0,0 +1,71 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Untitled Farm Game", "DarkTheme")
+
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("---------------------------")
+local run = game:GetService("RunService")
+local plr = game:GetService("Players").LocalPlayer
+local root = plr.Character.HumanoidRootPart
+
+_G.cfg = {
+    enabledAutoCollect = false,
+    enabledAutoSell = false,
+}
+
+local ownedplot = nil
+
+for i = 1, 6 do
+    local plot = workspace.Farms["Plot" .. i].OwnerSign.Hitbox.SurfaceGui.Title
+    if plot.ContentText == game.Players.LocalPlayer.Name then
+        ownedplot = i
+    end
+end
+
+if ownedplot == nil then
+    print("Player does not own any plot!")
+    return
+end
+wait(1)
+
+Section:NewToggle("Auto Collect", "", function(state)
+    _G.cfg.enabledAutoCollect = state
+end)
+
+Section:NewToggle("Auto Sell", "", function(state)
+    _G.cfg.enabledAutoSell = state
+end)
+_G.egg = "None"
+Section:NewDropdown("Eggs Selector", "", {"Chicken", "Pig", "Cow", "Sheep", "Donkey", "Horse", "Llama", "Goat"}, function(currentOption)
+    _G.egg = currentOption
+end)
+
+Section:NewButton("Buy Selected Egg", "", function()
+    local args = {
+        [1] = _G.egg
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Buy"):InvokeServer(unpack(args))
+end)
+
+Section:NewButton("Rejoin", "", function()
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+end)
+
+run.Heartbeat:Connect(function()
+    if not _G.cfg.enabledAutoCollect then return end
+    for penIndex, pen in pairs(workspace.Farms["Plot" .. ownedplot].Pens:GetChildren()) do
+        if pen:FindFirstChild("Drops") then
+            for _, v in pairs(pen.Drops:GetChildren()) do
+                local args = {
+                    [1] = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Drop"):WaitForChild("CollectDrop"),
+                    [2] = v.Name
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Drop"):InvokeServer(unpack(args))
+            end
+        end
+    end
+end)
+
+run.Heartbeat:Connect(function()
+    if not _G.cfg.enabledAutoSell then return end
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Sell"):InvokeServer()
+end)
