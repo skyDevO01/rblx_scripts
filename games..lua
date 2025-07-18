21cd464b9275e4235ed264b381800d88c88a5b8d
ngb <songhyedork@gmail.com>
2025-07-18 21:04:20 +0700

    Create games..lua

---
+++ games..lua
@@ -0,0 +1,11 @@
+return {
+    [14025779410] = {
+        name = "Avatar Fighting Simulator",
+        url = "https://raw.githubusercontent.com/skyDevO01/rblx_scripts/refs/heads/main/Avatar%20Fighting%20Simulator.lua"
+    },
+    
+    [88482905808841] = {
+        name = "Untitled Farm Game",
+        url = "https://raw.githubusercontent.com/skyDevO01/rblx_scripts/refs/heads/main/untitled%20farm%20game.lua"
+    },
+}
