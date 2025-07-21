1961bb60bb2f26a155d31737cd8eddbbe6c1d34e
ngb <songhyedork@gmail.com>
2025-07-21 21:12:47 +0700

    Update games..lua

--- games..lua
+++ games..lua
@@ -8,4 +8,8 @@ return {
         name = "Untitled Farm Game",
         url = "https://raw.githubusercontent.com/skyDevO01/rblx_scripts/refs/heads/main/untitled%20farm%20game.lua"
     },
+    [133322550157181] = {
+        name = "[BETA] Anime Evolution",
+        url = "https://raw.githubusercontent.com/skyDevO01/rblx_scripts/refs/heads/main/%5BBETA%5D%20Anime%20Evolution.lua"
+    },
 }
