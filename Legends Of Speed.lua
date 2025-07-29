3788e0e39e202df35bf3346b63d43d7e1b4cc00a
ngb <songhyedork@gmail.com>
2025-07-29 13:54:40 +0700

    Update Legends Of Speed.lua

--- Legends Of Speed.lua
+++ Legends Of Speed.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/skyDevO01/rblx_scripts/refs/heads/main/elerium-v2-ui-library.lua", true))()
 local window = library:AddWindow("@stopHurtsme / Legends Of Speed ⚡ / 3101667897", {
 	main_color = Color3.fromRGB(41, 74, 122),
 	min_size = Vector2.new(480, 406),
